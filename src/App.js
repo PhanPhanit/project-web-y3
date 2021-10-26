6c85b82186a523c1c8537a3c53f42d838cbb8460
PhaPhanit <phanphanit.pp12345@gmail.com>
2021-10-26 22:22:16 +0700

    react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import './App.css';
 import React from 'react';
-import {CartPage, Category, Home, Profile, ViewBook} from './pages';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import {CartPage, Category, Home, Profile, ViewBook} from './pages';
 
 
 function App() {
   return (
     <Router>
-      <Route path="/">
-        <Home />
-      </Route>
-      {/* <Route to="/categories">
-        <Category />
-      </Route> */}
-      {/* <Route to="/cart">
-        <CartPage />
-      </Route> */}
-      {/* <Route to="/profile">
-        <Profile />
-      </Route> */}
-      {/* <Route to="/viewbook/:id" children={<ViewBook />}></Route> */}
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+        <Route to="/categories">
+          <Category />
+        </Route>
+        <Route to="/cart">
+          <CartPage />
+        </Route>
+        <Route to="/profile">
+          <Profile />
+        </Route>
+        <Route to="/viewbook/:id" children={<ViewBook />}></Route>
+      </Switch>
     </Router>
   );
 }
