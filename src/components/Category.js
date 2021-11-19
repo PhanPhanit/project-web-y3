a160f076d046cb8928bc7ad9decfa3b338109f94
PhaPhanit <phanphanit.pp12345@gmail.com>
2021-11-19 09:09:17 +0700

    slide box

--- src/components/Category.js
+++ src/components/Category.js
@@ -3,8 +3,8 @@ import '../styles/components/category.css'
 
 const Category = ({title}) => {
     return (
-        <section className="wrapper-global category font-cat">
-            <h2>{title}</h2>
+        <section className="wrapper-global category">
+            <h2 className="font-cat">{title}</h2>
         </section>
     )
 }
